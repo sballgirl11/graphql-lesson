f17824a40d5c77c63d1566152593bbba787a82f4
Brittney Postma <brittney.postma@hotmail.com>
2019-07-19 07:58:00 -0400

    added item count to cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
-
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-	<div className="cart-icon" onClick={toggleCartHidden}>
-		<ShoppingIcon className="shopping-icon" />
-		<span className="item-count">{itemCount}</span>
-	</div>
+const CartIcon = ({toggleCartHidden, itemCount}) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <ShoppingIcon className="shopping-icon" />
+    {console.log(itemCount)}
+
+    <span className="item-count">{itemCount}</span>
+  </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-	itemCount: selectCartItemsCount,
-});
-
-export default connect(mapStateToProps)(CartIcon);
+export default CartIcon;
--- src/graphql/cart.utils.js
+++ src/graphql/cart.utils.js
@@ -6,12 +6,12 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if (existingCartItem) {
     return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        ? {...cartItem, quantity: cartItem.quantity + 1}
         : cartItem
     );
   }
 
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  return [...cartItems, {...cartItemToAdd, quantity: 1}];
 };
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
@@ -25,7 +25,13 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
 
   return cartItems.map(cartItem =>
     cartItem.id === cartItemToRemove.id
-      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      ? {...cartItem, quantity: cartItem.quantity - 1}
       : cartItem
   );
 };
+
+export const getCartItemCount = cartItems => cartItems =>
+  cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  );
